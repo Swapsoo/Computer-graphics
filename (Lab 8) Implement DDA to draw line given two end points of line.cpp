2420673ed4d32cf0410ee2139a2fd2b38a609ee4
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2023-08-14 16:05:22 +0545

    Lab 8

--- (Lab 8) Implement DDA to draw line given two end points of line.cpp
+++ (Lab 8) Implement DDA to draw line given two end points of line.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int WIN_WIDTH = 800;
 int WIN_HEIGHT = 600;
 
-// LAB 8 - DRAWING A LINE WITH DDA METHOD
+
 void drawLineDDA() {
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3d(0, 0, 0);
@@ -37,7 +37,7 @@ void drawLineDDA() {
 void myInit() {
     glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutCreateWindow("LAB 8 (ABHIYAN ACHARYA) - Drawing a line using Direct Method");
+    glutCreateWindow("Lab 8 - Swapnil Lamsal");
     glClearColor(1, 1, 1, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     glColor3d(0, 0, 0);
