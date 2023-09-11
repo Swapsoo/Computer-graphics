789198936eb9a1dc84611fb4db13ea13bd985d03
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2023-09-11 21:43:08 +0545

    Lab 8

---
+++ (Lab 8) Implement DDA to draw line given two end points of line.cpp
@@ -0,0 +1,51 @@
+#include <GL/glut.h>
+#include <cmath>
+#include <iostream>
+using namespace std;
+float xf, yf, xi, yi;
+void drawLine()
+{
+ float steps, dx = xf - xi, dy = yf - yi;
+ if (abs(dx) < abs(dy))
+ {
+ steps = abs(dy);
+ }
+ else
+ {
+ steps = abs(dx);
+ }
+ float xIncrement = dx / steps;
+ float yIncrement = dy / steps;
+ float x = xi;
+ float y = yi;
+ glClear(GL_COLOR_BUFFER_BIT);
+ glColor3f(0.0, 0.0, 0.0);
+ glBegin(GL_POINTS);
+ glVertex2f(round(x), round(y));
+ for (int i = 0; i <= steps; i++)
+ {
+ x += xIncrement;
+ y += yIncrement;
+ glVertex2f(round(x), round(y));
+}
+ glEnd();
+ glFlush();
+}
+int main(int argc, char **argv)
+{
+ cout << "Enter the initial point (x1, y1): ";
+ cin >> xi >> yi;
+ cout << "Enter the final point (x2, y2): ";
+ cin >> xf >> yf;
+ glutInit(&argc, argv);
+ glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+ glutInitWindowSize(500, 500);
+ glutInitWindowPosition(200, 200);
+ glutCreateWindow("Lab 8 Swapnil Lamsal");
+ glClearColor(1.0, 1.0, 1.0, 0.0);
+ glMatrixMode(GL_PROJECTION);
+ gluOrtho2D(0.0, 500.0, 0.0, 500.0);
+ glutDisplayFunc(drawLine);
+ glutMainLoop();
+ return 0;
+}
