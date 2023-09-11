b26e59f77efd88dd0a2b18458dfd37e8e7acd6ee
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2023-09-11 15:57:56 +0545

    Lab 19

--- (Lab 19) Write an OPENGL app to show clipping using orthogonal projection.cpp
+++ (Lab 19) Write an OPENGL app to show clipping using orthogonal projection.cpp
@@ -26,7 +26,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(400, 400);
-    glutCreateWindow("Lab 18 - Swapnil Lamsal");
+    glutCreateWindow("Lab 19 - Swapnil Lamsal");
     glutDisplayFunc(display);
     init();
     glutMainLoop();
