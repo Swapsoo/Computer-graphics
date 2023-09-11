1ddfe76e526350418e57a7f96459590ea3e29bdf
Swapsoo <134841328+Swapsoo@users.noreply.github.com>
2023-09-11 14:12:45 +0545

    Lab 19

---
+++ (Lab 19)Write an OPENGL app to show rectangle in two different viewports.cpp
@@ -0,0 +1,47 @@
+#include <GL/glut.h>
+
+void displayViewport1() {
+    glViewport(0, 0, 400, 400);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(1.0, 0.0, 0.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(-0.5, 0.5);
+    glEnd();
+
+    glFlush();
+}
+
+void displayViewport2() {
+    glViewport(400, 0, 400, 400);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glColor3f(0.0, 0.0, 1.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(-0.5, -0.5);
+    glVertex2f(0.5, -0.5);
+    glVertex2f(0.5, 0.5);
+    glVertex2f(-0.5, 0.5);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(1024, 768);
+    glutCreateWindow("LAB 19 Swapnil Lamsal");
+
+    glutDisplayFunc(displayViewport1);
+    glutDisplayFunc(displayViewport2);
+
+    glutMainLoop();
+
+    return 0;
+}
